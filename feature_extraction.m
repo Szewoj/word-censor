784faf4905d3a7a7b74abcb1e8069542178d1798
marcint2 <58732971+marcint2@users.noreply.github.com>
2022-12-18 14:04:55 +0100

    poprawione feature extraction i dodane dane

--- feature_extraction.m
+++ feature_extraction.m
@@ -1,10 +1,18 @@
-ADS = audioDatastore("samples/cat/")
+
 i=1;
-E=[];
-sigma=[];
-while ADS.hasdata
-    [audioIn,info] = read(ADS);
-    [coeffs,delta,deltaDelta,loc] = mfcc(audioIn,info.SampleRate);
-    E = [E;mean(coeffs)];
-    sigma = [sigma;std(coeffs)];
+types = ["samples/cat/","samples/one/","samples/zero/","samples/follow/","samples/forward/","samples/bird/","samples/visual/","samples/up/"];
+while i < 9
+    ADS = audioDatastore(types(i));
+    E=[];
+    sigma=[];
+    j=1;
+    while ADS.hasdata
+        [audioIn,info] = read(ADS);
+        [coeffs,delta,deltaDelta,loc] = mfcc(audioIn,info.SampleRate);
+        E(i,j,:) = [mean(coeffs),mean(delta),mean(deltaDelta)];
+        sigma(i,j,:) = [std(coeffs),std(delta),std(deltaDelta)];
+        j=j+1;
+    end
+    j=1;
+    i=i+1;
 end
